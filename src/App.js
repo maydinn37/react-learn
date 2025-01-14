8600d89b08ba73498add7a756a656a9d4a8c5c89
maydinn37 <mertaydinn37@gmail.com>
2025-01-14 10:06:15 +0300

    Four Lesson

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from "./logo.svg";
 import styles from "./App.module.css";
 import Test from "./test.js";
-import { Title } from "./companents.js";
+import { Title } from "./component.js";
 import Bootstrap from "./bootstrap.js";
+const Todos = ["todo1", "todo2", "todo3"];
 
 function App() {
   console.log(styles);
@@ -23,6 +24,36 @@ function App() {
         </>
       )}
       <Bootstrap />
+      <Title theme="dark">4.Ders</Title>
+      <main>
+        <h1
+          id="main"
+          className="test"
+          tabIndex={3}
+          style={{
+            color: "red",
+            backgroundColor: "grey",
+            paddingBottom: "10px",
+            paddingTop: "10px",
+            textAlign: "center",
+          }}
+        >
+          MydınDevelop
+        </h1>
+        <label
+          htmlFor="search"
+          tabIndex={2}
+          onClick={() => alert("Arama Yapıldı")}
+        >
+          Arama
+        </label>
+        <input type="text" id="search" tabIndex={1} />
+        <ul>
+          {Todos.map((todo, key) => (
+            <li key="index">{todo}</li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
