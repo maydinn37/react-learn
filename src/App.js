8d769d2647eaf1ac3ac9c07995b063618c125486
maydinn37 <mertaydinn37@gmail.com>
2025-01-17 11:42:15 +0300

    seven lesson Updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Bootstrap from "./bootstrap.js";
 import Btn from "./components/Btn.js";
 import "./components/tailwind.css";
 import Tab from "./components/Tab.js";
-import { useState, useRef } from "react";
+import { useState, useRef, useReducer } from "react";
+import todoReducer from "./reducers/todoReducer.js";
 import TestComponent from "./components/TestComponent.js";
 
 const Todos = ["todo1", "todo2", "todo3"];
@@ -16,7 +17,23 @@ function App() {
   const [show, setShow] = useState(false);
   const inputRef = useRef();
   const focusInput = () => inputRef.current.focus();
+  const [state, dispatch] = useReducer(todoReducer, {
+    todos: [],
+    todo: "",
+  });
 
+  const submitHandel = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_TODO",
+      todo: state.todo,
+    });
+  };
+  const onChange = (e) =>
+    dispatch({
+      type: "SET_TODO",
+      value: e.target.value,
+    });
   return (
     <div className={styles.App}>
       <Title>{process.env.NODE_ENV}</Title>
@@ -58,8 +75,8 @@ function App() {
         </label>
         <input type="text" id="search" tabIndex={1} />
         <ul>
-          {Todos.map((todo, key) => (
-            <li key="index">{todo}</li>
+          {Todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
           ))}
         </ul>
       </main>
@@ -96,9 +113,24 @@ function App() {
           type="text"
           ref={inputRef}
         />
-        <button className="ml-2" onClick={focusInput}>
+        <button className="ml-2 btn btn-dark" onClick={focusInput}>
           Focusla
         </button>
+      </div>
+      <Title theme="dark">8. Ders</Title>
+      <div>
+        <h1>Todo App</h1>
+        <form onSubmit={submitHandel}>
+          <input type="text" value={state.todo} onChange={onChange} />
+          <button className="btn btn-dark" disabled={!state.todo} type="submit">
+            Ekle
+          </button>
+        </form>
+        <ul>
+          {state.todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,17 @@
+function todoReducer(state, action) {
+  switch (action.type) {
+    case "SET_TODO":
+      return {
+        ...state,
+        todo: action.value,
+      };
+    case "ADD_TODO":
+      return {
+        ...state,
+        todo: "",
+        todos: [...state.todos, action.todo],
+      };
+  }
+}
+
+export default todoReducer;
